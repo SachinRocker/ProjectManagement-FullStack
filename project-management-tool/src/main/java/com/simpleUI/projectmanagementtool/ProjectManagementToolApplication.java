f10dad6a9f1ff4244dfc2a7d6c888ac8bc79edb9
sachin <sachinraj245@gmail.com>
2020-11-26 23:53:58 +0530

    first-commit

---
+++ project-management-tool/src/main/java/com/simpleUI/projectmanagementtool/ProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.simpleUI.projectmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolApplication.class, args);
+	}
+
+}
---
+++ project-management-tool/src/test/java/com/simpleUI/projectmanagementtool/ProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleUI.projectmanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
