7d7e6a7a6ad7bd096da1c20549c522727bb40ffe
sachin <sachinraj245@gmail.com>
2020-12-03 08:04:12 +0530

    added controller,services,repository,exceptionhandler

---
+++ project-management-tool/src/main/java/com/simpleUI/projectmanagementtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.simpleUI.projectmanagementtool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.simpleUI.projectmanagementtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Integer> {
+
+	Project findByprojectIdentifer(String projectId);
+
+}
